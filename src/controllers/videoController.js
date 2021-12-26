86868e7ac4ede58e14ba706c2ac3b7b0e0236a3e
Yoon Wookji <wookjl.yoon@gmail.com>
2021-12-26 23:16:06 +0900

    첫번째 Comiit PUg 이전

---
+++ src/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const trending = (req, res) => res.send("Home Page Videos");
+
+export const see = (req, res) => res.send("See Video");
+
+export const edit = (req, res) => res.send("Edit");
+export const remove = (req, res) => res.send("Remove Video");
+export const upload = (req, res) => res.send("Upload Video");
+
+export const search = (req, res) => res.send("Search");
