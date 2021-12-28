115019e884e437362aa9180a2e7ef5065a5416b4
Yoon Wookji <wookjl.yoon@gmail.com>
2021-12-29 00:16:44 +0900

    Class #5.4 pug 도입

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,8 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-
-export const see = (req, res) => res.send("See Video");
-
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) =>
+  res.render("home.pug", { pageTitle: "Home" });
+export const see = (req, res) =>
+  res.render("watch.pug", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit.pug", { pageTitle: "Edit" });
 export const remove = (req, res) => res.send("Remove Video");
 export const upload = (req, res) => res.send("Upload Video");
-
 export const search = (req, res) => res.send("Search");
--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
+
 const app = express();
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
 const logger = morgan("dev");
 app.use(logger);
 
